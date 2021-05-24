b17594a541d3ee984ccdf60be9d6128766e4dadc
CoderNia <43446760+CoderNia@users.noreply.github.com>
2021-05-25 02:08:25 +0900

    Add files via upload
    제출한 코드에서 지연시간만 다름 (이미지 지연 시간)

--- HYE_UI.py
+++ HYE_UI.py
@@ -12,17 +12,23 @@ import zoom_zoom
 class Ui_HYE(object):
     def setupUi(self, HYE):
         HYE.setObjectName("HYE")
-        HYE.resize(284, 89)
+        HYE.resize(437, 89)
         self.small_screen = QtWidgets.QRadioButton(HYE)
-        self.small_screen.setGeometry(QtCore.QRect(30, 20, 108, 19))
+        self.small_screen.setGeometry(QtCore.QRect(160, 20, 108, 19))
         self.small_screen.setObjectName("small_screen")
         self.big_screen = QtWidgets.QRadioButton(HYE)
-        self.big_screen.setGeometry(QtCore.QRect(30, 60, 108, 19))
+        self.big_screen.setGeometry(QtCore.QRect(10, 60, 108, 19))
         self.big_screen.setObjectName("big_screen")
         self.start_button = QtWidgets.QPushButton(HYE)
-        self.start_button.setGeometry(QtCore.QRect(160, 40, 93, 28))
+        self.start_button.setGeometry(QtCore.QRect(320, 30, 93, 28))
         self.start_button.setObjectName("start_button")
         self.start_button.clicked.connect(self.start_button_clicked)
+        self.smallest_screen = QtWidgets.QRadioButton(HYE)
+        self.smallest_screen.setGeometry(QtCore.QRect(10, 20, 141, 19))
+        self.smallest_screen.setObjectName("smallest_screen")
+        self.biggest_screen = QtWidgets.QRadioButton(HYE)
+        self.biggest_screen.setGeometry(QtCore.QRect(160, 60, 141, 19))
+        self.biggest_screen.setObjectName("biggest_screen")
 
         self.retranslateUi(HYE)
         QtCore.QMetaObject.connectSlotsByName(HYE)
@@ -33,12 +39,19 @@ class Ui_HYE(object):
         self.small_screen.setText(_translate("HYE", "작은 모니터"))
         self.big_screen.setText(_translate("HYE", "큰 모니터"))
         self.start_button.setText(_translate("HYE", "시작"))
+        self.smallest_screen.setText(_translate("HYE", "더 작은 모니터"))
+        self.biggest_screen.setText(_translate("HYE", "더 큰 모니터"))
+
 
     def start_button_clicked(self):
-        if self.big_screen.isChecked():
+        if self.biggest_screen.isChecked():
             zoom_zoom.zoom_exec(5)
-        else:
+        elif self.big_screen.isChecked():
             zoom_zoom.zoom_exec(4)
+        elif self.small_screen.isChecked():
+            zoom_zoom.zoom_exec(3)
+        else:
+            zoom_zoom.zoom_exec(2)
 
 if __name__ == "__main__":
     import sys
